97f076b2c8ad0a2a16d8bba35a8e395f09fa883c
PabloHenrique <pablohenriquenadai@gmail.com>
2022-01-11 16:18:31 -0300

    Salvando modificações V.01

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-alert('Olá!')
+function insert(num){
+    var numero = document.getElementById('appear').innerHTML
+    document.getElementById('appear').innerHTML = numero + num
+}
+
+function limpar(){
+    document.getElementById('appear').innerHTML = ''
+}
+
+function calcular(){
+
+}
+
+/*
+var txtn1 = document.getElementById('txtn1')
+n1 = Number(txtn1.value)
+
+var txtn2 = document.getElementById('txtn2')
+n2 = Number(txtn2.value)
+
+var txtn3 = document.getElementById('txtn3')
+n3 = Number(txtn3.value)
+
+var txtn4 = document.getElementById('txtn4')
+n4 = Number(txtn4.value)
+
+var txtn5 = document.getElementById('txtn5')
+n5 = Number(txtn5.value)
+
+var txtn6 = document.getElementById('txtn6')
+n6 = Number(txtn6.value)
+
+var txtn7 = document.getElementById('txtn7')
+n7 = Number(txtn7.value)
+
+var txtn8 = document.getElementById('txtn8')
+n8 = Number(txtn8.value)
+
+var txtn9 = document.getElementById('txtn9')
+n9 = Number(txtn9.value)
+
+var txtn0 = document.getElementById('txtn0')
+n0 = Number(txtn0.value)
+
+appear.innerText = `${n7}`
+*/
